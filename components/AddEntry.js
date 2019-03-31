79cf0ce604969d8cae7369746246eab3ead83c65
Vasili <anoshin45@gmail.com>
2019-03-31 14:11:12 -0700

    AddEntry.js

---
+++ components/AddEntry.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { View } from 'react-native'	import { View } from "react-native";
+import { getMetricMetaInfo } from '../utils/helpers'	import { getMetricMetaInfo } from "../utils/helpers";
+
+
+ export default class AddEntry extends Component {	export default class AddEntry extends Component {
+  render() {	  render() {
+    return (	    return <View>{getMetricMetaInfo("bike").getIcon}</View>;
