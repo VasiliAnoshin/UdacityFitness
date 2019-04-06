6d35b8b02f994b186619acb3f45d177e86d28276
Vasili <anoshin45@gmail.com>
2019-04-05 23:18:43 -0700

    changes in Navigation

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+
+import TabNav from './components/TabNav';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <History />
+          <TabNav />
         </View>
       </Provider>
     )
